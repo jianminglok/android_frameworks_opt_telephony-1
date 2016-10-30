171b6769130e947fc201ec3f504a0079ec6159ef
Lok Jian Ming <jianminglok@hotmail.com>
2016-10-30 12:11:10 +0800

    Update BaseCommands.java

--- src/java/com/android/internal/telephony/BaseCommands.java
+++ src/java/com/android/internal/telephony/BaseCommands.java
@@ -838,6 +838,14 @@ public abstract class BaseCommands implements CommandsInterface {
     }
 
     /**
+     * @hide
+     */
+    @Override
+    public int getLteOnGsmMode() {
+        return TelephonyManager.getLteOnGsmModeStatic();
+    }
+    
+    /**
      * {@inheritDoc}
      */
     @Override
